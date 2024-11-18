99cd3913caf09a157bca3e80d2f9345ece83fb17
Umut556 <umutduran221@gmail.com>
2024-11-18 13:46:22 +0300

    aa

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import loginSlice from './loginSlice'
 import calisanSlice from './calisanSlice'
+import magazaSlice from './magazaSlice'
 
 export const store = configureStore({
     reducer: {
         login: loginSlice,
-        calisan: calisanSlice
+        calisan: calisanSlice,
+        magaza: magazaSlice
     },
 })
