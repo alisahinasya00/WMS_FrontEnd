a55ef8548b92610858fa3772451cec98ebdd6444
Umut556 <umutduran221@gmail.com>
2024-11-18 13:22:34 +0300

    aa

--- src/redux/calisanSlice.jsx
+++ src/redux/calisanSlice.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
@@ -22,7 +21,7 @@ export const deleteCalisan = createAsyncThunk('calisan/deleteCalisan', async (ca
 });
 
 export const updateCalisan = createAsyncThunk('calisan/updateCalisan', async ({ calisanId, updatedData }) => {
-    const response = await axios.put(`${API_URL}/`, updatedData);
+    const response = await axios.put(`${API_URL}/${calisanId}`, updatedData);
     return response.data;
 });
 
